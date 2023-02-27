9e599aa275e8fde922b28088770f7a8e3a953180
SukhrabTurdiyev <sukhrab.turdiyev@nu.edu.kz>
2023-02-27 14:04:59 +0600

    lab6

---
+++ main.js
@@ -0,0 +1,23 @@
+//java script file//
+//fist problem (1)//
+function printHello() {
+  console.log("Hello, World!");
+  console.log("Hello, World!");
+  console.log("Hello, World!");
+}
+
+//second problem(4)//
+function greet(name) {
+  return `Hi, ${name}!`;
+}
+
+//third problem (10)//
+function areEqual(x, y) {
+  if (x === y) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
